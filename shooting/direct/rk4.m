f2a76edfe86d93e52106b3a1d0ee0b59c06a8100
Martin Pryde <martinpryde@ymail.com>
2024-01-19 20:42:51 +0100

    Add new files and ignore untracked files

---
+++ shooting/direct/rk4.m
@@ -0,0 +1,13 @@
+function x_dot = rk4(f,x,u,dt)
+    arguments
+        f (1,1) function_handle
+        x (:,1) double
+        u (:,1) double
+        dt (1,1) double
+    end
+    k1 = f(x,u);
+    k2 = f(x + k1*dt/2,u);
+    k3 = f(x + k2*dt/2,u);
+    k4 = f(x + k3*dt,u);
+    x_dot = x + (k1 + 2*k2 + 2*k3 + k4)*dt/6;
+end
