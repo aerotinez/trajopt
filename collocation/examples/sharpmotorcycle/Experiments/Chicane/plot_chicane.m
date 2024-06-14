36e917b534b12821ec81c4428a88e2b72f614979
Martin Pryde <martin.pryde@universite-paris-saclay.fr>
2024-06-14 18:40:31 +0200

    chore: Update plot_chicane.m to save figures in EPS format

--- collocation/examples/sharpmotorcycle/Experiments/Chicane/plot_chicane.m
+++ collocation/examples/sharpmotorcycle/Experiments/Chicane/plot_chicane.m
@@ -2,9 +2,9 @@ close("all"); clear; clc;
 load("chicane_results.mat");
 fig_path = "C:\Users\marti\PhD\Articles\CDC24\Figures\";
 fig_results = plotResults(results);
-saveas(fig_results,fig_path + "results_chicane",'epsc');
+% saveas(fig_results,fig_path + "results_chicane",'epsc');
 
 fig_road = plotScenario(chicaneScenario(),results);
 view(fig_road.Children(end),-90,4);
 title("Chicane scenario","Parent",fig_road.Children(end));
-saveas(fig_road,fig_path + "road_chicane",'epsc');
+% saveas(fig_road,fig_path + "road_chicane",'epsc');
